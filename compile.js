51268b7d7e9bf2f610ae5c23e9859d297b7fe95e
Keller18306 <kompeter17@gmail.com>
2021-09-21 02:46:45 +0300

    compiler

---
+++ compile.js
@@ -0,0 +1,11 @@
+const { execSync } = require('child_process')
+const { writeFileSync, existsSync, copyFileSync, rmSync } = require('fs')
+
+void function main() {
+    if (existsSync('./build/')) rmSync('./build/', { recursive: true })
+    console.log(execSync('tsc').toString())
+    const package = require('./package.json')
+    package.main = 'index.js'
+    writeFileSync('./build/package.json', JSON.stringify(package, null, 4))
+    copyFileSync('./package-lock.json', './build/package-lock.json')
+}()
