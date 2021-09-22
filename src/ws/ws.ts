37934a67f1303b6b55eb3ed8d2d01d6da19c6638
Keller18306 <kompeter17@gmail.com>
2021-09-22 20:36:27 +0300

    опкоды в отдельный файл

--- src/ws/ws.ts
+++ src/ws/ws.ts
@@ -3,6 +3,7 @@ import { formatURL, isJSON, timePromise } from './utils';
 
 import { default as WebSocket } from 'ws'
 import * as Types from '../api/types';
+import { WSOpcodes } from './opcodes';
 
 export class WSAPIError extends Error {
     readonly code: string;
@@ -87,32 +88,6 @@ type InitType = {
         percent: number,
         order: number
     }[]
-}
-
-const WSOpcodes = {
-    COMPLETE: 'C',
-    ERROR: 'R',
-    INCOMING_TRANSACTION: 'TR',
-    UPDATE_BALANCE: 'TZ',
-    INIT: 'INIT',
-    NEW_MERCHANT: 'NM',
-    TRANSACTION: 'T',
-    PUSH: 'P',
-    ALREADY_CONNECTED: 'ALREADY_CONNECTED',
-    BROKEN: 'BROKEN',
-    TOP: 'P',
-    BOT_CLICK: 'BC', //stats
-    BOT_SHOW: 'BS', //stats
-    LOAD_GROUP: 'G',
-    GET_RICH: 'GR', //don't used (removed in coin from 01.08.2021)
-    SET_RICH: 'SR', //don't used (removed in coin from 01.08.2021)
-    GET_TRANSCATIONS: 'TX',
-    GET_SCORE: 'GS',
-    GET_MY_PLACE: 'X',
-    GET_GIFT: 'GG',
-    SYNC_TX_LIST: 'SY',
-    TRACK_TYPE: 'TS', //I DON'T NOW | TO DO
-    BUY_ITEM_BY_ID: 'B' //don't used after close mining
 }
 
 export declare interface VKCoinWebSocket {
